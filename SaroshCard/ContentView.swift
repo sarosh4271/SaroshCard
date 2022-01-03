86dfda241df9ef13e77823089a2c8913f5e3df70
sarosh4271 <74011881+sarosh4271@users.noreply.github.com>
2022-01-03 12:25:55 +0500

    Initial Commit

---
+++ SaroshCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SaroshCard
+//
+//  Created by Sarosh Tahir on 03/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SaroshCard/SaroshCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaroshCardApp.swift
+//  SaroshCard
+//
+//  Created by Sarosh Tahir on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SaroshCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
